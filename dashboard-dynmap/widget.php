2b1f5864e7651afa5add685ac82a590eb6cfede7
Xander Smeets <xander.smeets@outlook.com>
2014-06-02 21:13:00 +0200

    Update widget.php

--- dashboard-dynmap/widget.php
+++ dashboard-dynmap/widget.php
@@ -2,8 +2,7 @@
 /**
  * This file could be used to catch submitted form data. When using a non-configuration
  * view to save form data, remember to use some kind of identifying field in your form.
+ * Changed by XSmeets at 2-6-2014.
  */
 ?>
-<p>This is an example dashboard widget!</p>
-<p>This is the front-facing part of the widget, and can be found and edited from <tt><?php echo __FILE__ ?></tt></p>
-<p>Widgets can be configured as well. Currently, this is set to <b><?php echo self::get_dashboard_widget_option(self::wid, 'example_number'); ?></b> ! To change the number, hover over the widget title and click on the "Configure" link.</p>
+<iframe src="self::wid" width="426" height="130"></iframe>
