84f80cb252e6e86f9364fb94614234dae996bf00
Xander Smeets <xander.smeets@outlook.com>
2014-06-02 21:10:25 +0200

    Update map-config.php

--- dashboard-dynmap/map-config.php
+++ dashboard-dynmap/map-config.php
@@ -1,8 +1,4 @@
 <?php
-/**
- * This file could be used to catch submitted form data. When using a non-configuration
- * view to save form data, remember to use some kind of identifying field in your form.
- */
     $number = stripslashes($_POST['number']);
     self::update_dashboard_widget_options(
             self::wid,                                  //The  widget id
@@ -15,3 +11,6 @@
 <p>This is an example dashboard widget!</p>
 <p>This is the configuration part of the widget, and can be found and edited from <tt><?php echo __FILE__ ?></tt></p>
 <input type="text" name="number" />
+/**
+ * Changed by Xander Smeets at 2-6-2014
+ */
