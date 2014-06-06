e46131b0e99b274d1ae0a45e5800c74401b3dd9e
Xander Smeets <xander.smeets@outlook.com>
2014-06-06 15:06:28 +0200

    Update widget.php

--- dashboard-dynmap/widget.php
+++ dashboard-dynmap/widget.php
@@ -2,8 +2,8 @@
 /**
  * This file could be used to catch submitted form data. When using a non-configuration
  * view to save form data, remember to use some kind of identifying field in your form.
- * Changed by XSmeets at 2-6-2014.
- * Changed by XSmeets at 6-6-2014.
+ * Changed by Xander Smeets at 2-6-2014.
+ * Changed by Xander Smeets at 6-6-2014.
  */
 ?>
 <iframe src="<?php echo self::get_dashboard_widget_option(self::wid, 'map_url'); ?>" width="426" height="130"></iframe>
