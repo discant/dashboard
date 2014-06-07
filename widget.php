3402b3c7a2849a15fcda12738104e136c3b5ff34
Xander Smeets <xander.smeets@outlook.com>
2014-06-07 11:02:05 +0200

    Made widget larger

--- widget.php
+++ widget.php
@@ -4,6 +4,7 @@
  * view to save form data, remember to use some kind of identifying field in your form.
  * Changed by Xander Smeets at 2-6-2014.
  * Changed by Xander Smeets at 6-6-2014.
+ * Changed by Xander Smeets at 7-6-2014.
  */
 ?>
-<iframe src="<?php echo self::get_dashboard_widget_option(self::wid, 'map_url'); ?>" width="426" height="130"></iframe>
+<iframe src="<?php echo self::get_dashboard_widget_option(self::wid, 'map_url'); ?>" width="426" height="250"></iframe>
